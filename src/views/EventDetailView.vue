9464c171f54c0759df519ee5a5b8b7c6e87143b6
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-08-07 18:23:42 +0700

    1.9

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, defineProps } from 'vue'
 import Event from '@/types/Event'
+import EventService from '@/services/EventService'
+
 const event = ref<Event>(null)
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  }
+})
+
 onMounted(() => {
-  // fetch event (by id) and set local event data
+  EventsService.getEvent(props.id)
+    .then((response) => {
+      event.value = response.data
+    })
+    .catch(error => {
+      console.error('There was an error!', error)
+    })
 })
 </script>
+
 <template>
-    <div>
+    <div v-if="event">
         <h1>{{ event.title }}</h1>
         <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
         <p>{{ event.description }}</p>

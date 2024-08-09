781dedae1aebfa1c25d1a0fc71704f59ed5945ab
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-08-09 07:31:27 +0700

    4.2

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -8,9 +8,16 @@ const props = defineProps<{
 }>()
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
-
+const router = useRouter()
+const register = () => {
+  // If tthe registeration API call successful
+  // Push back to the event detajls view
+  router.push({ name: 'event-detail-view'})
+}
 </script>
 
 <template>
   <p>Register event here</p>
+  <aside class="ai-send"></aside>
+  <button @click="register">Register</button>
 </template>
